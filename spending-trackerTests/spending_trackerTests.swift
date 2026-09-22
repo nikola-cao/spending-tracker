a2937b065498e76845b1924811c9ae077a8c4c80
nikola-cao <cao.nikola.a@gmail.com>
2026-09-21 22:13:27 -0400

    init

---
+++ spending-trackerTests/spending_trackerTests.swift
@@ -0,0 +1,19 @@
+//
+//  spending_trackerTests.swift
+//  spending-trackerTests
+//
+//  Created by Nikola Cao on 9/21/26.
+//
+
+import Testing
+@testable import spending_tracker
+
+struct spending_trackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
